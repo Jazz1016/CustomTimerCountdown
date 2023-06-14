aadb367212b3ac5d2ea49cfd7834c812fbcb200b
James Lea <james.q.lea@gmail.com>
2023-06-13 18:29:34 -0600

    first commit

---
+++ CustomTimerCountdown/CustomTimerCountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTimerCountdownApp.swift
+//  CustomTimerCountdown
+//
+//  Created by James Lea on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomTimerCountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
